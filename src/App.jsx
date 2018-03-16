b3f7d2ba984db63abefb8807c44365d4dc7cbebd
Harshal Ringe <harshal.r@247software.in>
2018-03-16 19:17:49 +0530

    Component added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import asyncComponent from './asyncComponent.jsx';
 const Component1 = asyncComponent(() => import('./Component1.jsx').then(module => module.default), {});
 const Component2 = asyncComponent(() => import('./Component2.jsx').then(module => module.default), {});
 const Component3 = asyncComponent(() => import('./Component3.jsx').then(module => module.default), {});
+const Component4 = asyncComponent(() => import('./Component4.jsx').then(module => module.default), {});
 
 
 export default class App extends Component {
@@ -17,11 +18,13 @@ export default class App extends Component {
 				<div id="app-content" className="app-content">
 					<Link to='/'>Component1</Link> &nbsp;
 					<Link to='/Component2'>Component2</Link> &nbsp;
-					<Link to='/Component3'>Component3</Link>
+					<Link to='/Component3'>Component3</Link> &nbsp;
+					<Link to='/Component4'>Component4</Link>
 					<div id="main-content" className="main-content">
 						<Route exact path="/" component={Component1} />
 						<Route exact path="/Component2" component={Component2} />
 						<Route exact path="/Component3" component={Component3} />
+						<Route exact path="/Component4" component={Component4} />
 					</div>
 				</div>
 			</Router>
---
+++ src/Component4.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Component4 extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>
+                Component 4
+            </h1>
+        );
+    }
+}            
